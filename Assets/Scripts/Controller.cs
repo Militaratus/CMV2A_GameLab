fa1313cf47706c2bc56a720cba061f8d44792033
FightAH <34405770+FightAH@users.noreply.github.com>
2018-02-12 17:33:14 +0100

    Vr camera and controls
    Vr camera and controls

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)GetComponent<SteamVR_TrackedObject>().index);
+        }
+    }
+}
