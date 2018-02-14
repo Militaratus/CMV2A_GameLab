ab6745d9c0734f750197bef47290305a3f4af04e
Militaratus <jarednealon@outlook.com>
2018-02-14 14:45:47 +0100

    Opening the door

---
+++ Assets/Scripts/Doors/LockedDoor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    public bool flipped = false;
+    public bool rotated = false;
+
+    private float sideFlip = -1;
+    private float side = -1;
+    private float smooth = 270.0f;
+    private float doorOpenAngle = -90f;
+    private bool open = false;
+
+    private Vector3 defaultRotation;
+    private Vector3 openRotation;
+
+    // Use this for initialization
+    private void Start ()
+    {
+        defaultRotation = transform.eulerAngles;
+        SetRotation();
+        sideFlip = (flipped ? 1 : -1);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (open)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(openRotation), Time.deltaTime * smooth);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        SetDoorRotation(transform.position);
+        SetRotation();
+        open = !open;
+    }
+
+    private void SetRotation()
+    {
+        openRotation = new Vector3(defaultRotation.x, defaultRotation.y + (doorOpenAngle * (sideFlip * side)), defaultRotation.z);
+    }
+
+    private void SetDoorRotation(Vector3 interacterPosition)
+    {
+        side = ((rotated == false && interacterPosition.z > transform.position.z) || (rotated == true && interacterPosition.x > transform.position.x) ? -1 : 1);
+    }
+
+    
+}
