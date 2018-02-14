f69a7d490122b2bdb45fc0d0c684e11d6767ebb5
Militaratus <jarednealon@outlook.com>
2018-02-14 15:46:08 +0100

    Code Note Cube Added

---
+++ Assets/Scripts/Objects/NoteCode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+
+public class NoteCode : VRTK_InteractableObject
+{
+    [Header("Code Note Options")]
+    public string passcode;
+
+    private Text element;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        SetGUIKey();
+    }
+
+    // Apply the key text to the GUI of the button
+    void SetGUIKey()
+    {
+        if (!element)
+        {
+            element = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+        }
+
+        element.text = passcode;
+    }
+}
