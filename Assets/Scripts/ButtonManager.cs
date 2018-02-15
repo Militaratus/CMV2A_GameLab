f4007f5dabb15e79f515469bdb0aae32ec581d11
FightAH <34405770+FightAH@users.noreply.github.com>
2018-02-15 12:06:34 +0100

    Working menu
    Menu buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+[System.Serializable]
+public class ButtonManager : MonoBehaviour {
+    //If button is pressed load scene.
+    public void MenuButton(string MainMenu)
+    {
+        Debug.Log("Pressed");
+    }
+    //If button is pressed quit game.
+    public void ExitButton()
+    {
+        Application.Quit();
+	}
+}
---
+++ Assets/Scripts/MenuToggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuToggle : MonoBehaviour {
+
+    public VRTK_ControllerEvents controllerEvents;
+    public GameObject menu;
+
+    bool menuState = false;
+
+    // Use ts for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnEnable()
+    {
+        controllerEvents.ButtonTwoReleased += ControllerEvents_ButtonTwoReleased;
+    }
+
+    void OnDisable()
+    {
+        controllerEvents.ButtonTwoReleased -= ControllerEvents_ButtonTwoReleased;
+    }
+
+    private void ControllerEvents_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        menuState = !menuState;
+        menu.SetActive(menuState);
+    }
+}
