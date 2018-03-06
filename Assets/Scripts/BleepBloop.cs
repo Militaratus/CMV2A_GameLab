4d2391ff0aee68dfba44ea26f89a33419b20a403
FightAH <34405770+FightAH@users.noreply.github.com>
2018-03-06 14:46:32 +0100

    Bleep bloop canvas
    Bleep bloop canvas

--- Assets/Scripts/BleepBloop.cs
+++ Assets/Scripts/BleepBloop.cs
@@ -8,15 +8,31 @@ public class BleepBloop : VRTK_InteractableObject
     public AudioClip Bleep;
     private AudioSource audioPlayer;
 
+    GameObject canvas;
+
+    public bool canvasActive = false;
+
     public override void StartUsing(VRTK_InteractUse usingObject)
     {
-        Debug.Log("Triggered");
+        base.StartUsing(usingObject);
         audioPlayer.clip = Bleep;
         audioPlayer.Play();
+        if (!canvasActive)
+        {
+            canvasActive = true;
+            canvas.SetActive(true);
+        }
+        else if (canvasActive)
+        {
+            Debug.Log("FUCK");
+            canvasActive = false;
+            canvas.SetActive(false);
+        }
     }
 
     protected void Start()
     {
         audioPlayer = GetComponent<AudioSource>();
+        canvas = transform.GetChild(0).gameObject;
     }
 }
