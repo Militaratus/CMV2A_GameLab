f35a389abd9a7040615db8ecbaae5220f6adee7f
FightAH <34405770+FightAH@users.noreply.github.com>
2018-03-06 13:16:56 +0100

    Bleep Bloop
    Bleep Bloop

---
+++ Assets/Scripts/BleepBloop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class BleepBloop : VRTK_InteractableObject
+{
+    public AudioClip Bleep;
+    private AudioSource audioPlayer;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        Debug.Log("Triggered");
+        audioPlayer.clip = Bleep;
+        audioPlayer.Play();
+    }
+
+    protected void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+}
