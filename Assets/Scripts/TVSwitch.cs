69f77be07c7c2129266d16f64473d686580f03a5
FightAH <34405770+FightAH@users.noreply.github.com>
2018-03-07 12:18:55 +0100

    tv telephone
    tv telephone

---
+++ Assets/Scripts/TVSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.Video;
+
+public class TVSwitch : VRTK_InteractableObject
+{
+
+    private VideoPlayer videoPlayer;
+
+    GameObject tv;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log("Using");
+        videoPlayer.Play();
+
+    }
+
+    protected void Start()
+    {
+       tv = transform.GetChild(0).gameObject;
+        videoPlayer = tv.GetComponent<VideoPlayer>();
+    }
+}
