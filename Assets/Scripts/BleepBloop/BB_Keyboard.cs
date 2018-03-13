2bb9885aa18e4ab4d2a53f602294a7f7d75c3b47
Militaratus <jarednealon@outlook.com>
2018-03-13 16:04:54 +0100

    BeepBoob MUTHAFUDGER!!!

---
+++ Assets/Scripts/BleepBloop/BB_Keyboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BB_Keyboard : MonoBehaviour
+{
+    public Text input;
+
+    public void ClickKey(string character)
+    {
+        input.text += character;
+    }
+
+    public void Backspace()
+    {
+        if (input.text.Length > 0)
+        {
+            input.text = input.text.Substring(0, input.text.Length - 1);
+        }
+    }
+
+    public void Enter()
+    {
+        input.text += "\n";
+    }
+}
--- Assets/Scripts/BleepBloop/BleepBloop.cs
+++ Assets/Scripts/BleepBloop/BleepBloop.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using VRTK;
 
 public class BleepBloop : VRTK_InteractableObject
@@ -8,31 +9,122 @@ public class BleepBloop : VRTK_InteractableObject
     public AudioClip Bleep;
     private AudioSource audioPlayer;
 
-    GameObject canvas;
+    internal Transform playerHead;
+    internal Transform displayHead;
 
-    public bool canvasActive = false;
+    GameObject menuCanvas;
+    GameObject cluebotCanvas;
+    GameObject noteCanvas;
+
+    Button cluebotButton;
+    Text cluebotText;
+
+    int cluebotBattery = 100;
+
+    bool canvasActive = false;
+    bool cluebotActive = false;
+
+    IEnumerator cluebotCoroutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        audioPlayer = GetComponent<AudioSource>();
+        audioPlayer.clip = Bleep;
+
+        playerHead = GameObject.FindGameObjectWithTag("Player").transform;
+        displayHead = transform.GetChild(0);
+
+        menuCanvas = displayHead.GetChild(0).gameObject;
+        cluebotCanvas = displayHead.GetChild(1).gameObject;
+        cluebotButton = cluebotCanvas.transform.GetChild(2).GetComponent<Button>();
+        cluebotText = cluebotButton.transform.GetChild(0).GetComponent<Text>();
+        noteCanvas = displayHead.GetChild(2).gameObject;
+
+        
+    }
 
     public override void StartUsing(VRTK_InteractUse usingObject)
     {
         base.StartUsing(usingObject);
-        audioPlayer.clip = Bleep;
-        audioPlayer.Play();
+
         if (!canvasActive)
         {
             canvasActive = true;
-            canvas.SetActive(true);
+            SwitchMenu("menu");
         }
         else if (canvasActive)
         {
-            Debug.Log("FUCK");
             canvasActive = false;
-            canvas.SetActive(false);
+            SwitchMenu("menu");
         }
     }
 
-    protected void Start()
+    public void SwitchMenu(string menu)
     {
-        audioPlayer = GetComponent<AudioSource>();
-        canvas = transform.GetChild(0).gameObject;
+        // Turn off everything
+        menuCanvas.SetActive(false);
+        cluebotCanvas.SetActive(false);
+        noteCanvas.SetActive(false);
+
+        if (menu == "menu")
+        {
+            menuCanvas.SetActive(true);
+        }
+        if (menu == "cluebot")
+        {
+            cluebotCanvas.SetActive(true);
+        }
+        if (menu == "note")
+        {
+            noteCanvas.SetActive(true);
+        }
+
+        audioPlayer.Play();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // Aim the Display to the player
+        displayHead.LookAt(playerHead.position);
+    }
+
+    public void ToggleCluebot()
+    {
+        cluebotButton.interactable = false;
+
+        if (cluebotCoroutine != null)
+        {
+            StopCoroutine(cluebotCoroutine);
+            cluebotCoroutine = null;
+        }
+
+        cluebotCoroutine = CluebotCoroutine();
+        StartCoroutine(cluebotCoroutine);
+    }
+
+    IEnumerator CluebotCoroutine()
+    {
+        while (cluebotBattery > 0)
+        {
+            cluebotBattery--;
+            cluebotText.text = cluebotBattery + "%";
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        Debug.Log("RECHARGING");
+
+        while (cluebotBattery < 100)
+        {
+            cluebotBattery++;
+            cluebotText.text = "RECHARGING...\n" + cluebotBattery + "%";
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        cluebotText.text = "Tap to Summon";
+        cluebotButton.interactable = true;
     }
 }
