0f4ebcaf160195d6c5f79ccf711446891425076f
FightAH <34405770+FightAH@users.noreply.github.com>
2018-03-13 13:15:10 +0100

    Standerd scene update
    Standerd scene update

---
+++ Assets/Scripts/LineUP.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class LineUP : VRTK_InteractableObject
+{
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        Debug.Log("Fuck");
+    }
+
+}
