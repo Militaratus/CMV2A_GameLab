65e93ec7645ffa7bbef7326670b3947f4c2da32d
Militaratus <jarednealon@outlook.com>
2018-03-19 15:58:47 +0100

    Allows an image to be displayed during HeadsetFade

---
+++ Assets/Scripts/Headset/HeadsetCollisionImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK;
+
+public class HeadsetCollisionImage : VRTK_HeadsetCollisionFade
+{
+    [Header("Salty Puppies - Custom Settings")]
+    public Image fadeMaterial;
+
+    protected override void OnHeadsetCollisionEnded(object sender, HeadsetCollisionEventArgs e)
+    {
+        base.OnHeadsetCollisionEnded(sender, e);
+        fadeMaterial.color = Color.clear;
+    }
+
+    protected override void StartFade()
+    {
+        base.StartFade();
+        fadeMaterial.color = Color.white;
+    }
+}
