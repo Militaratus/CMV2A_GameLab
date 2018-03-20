d473c4fdcb9bb400176efeb888f367649f244151
FightAH <34405770+FightAH@users.noreply.github.com>
2018-03-20 12:35:18 +0100

    Message to start case1
    Message to start case1

--- Assets/Scripts/Mirror/Mirror.cs
+++ Assets/Scripts/Mirror/Mirror.cs
@@ -8,7 +8,7 @@ public class Mirror : MonoBehaviour {
     public Transform PlayerCam;
 
 	// Use this for initialization
-	void Start ()
+	void OnAwake ()
     {
         PlayerCam = Camera.main.transform;
 
---
+++ Assets/Scripts/MissionTV.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class MissionTV : MonoBehaviour {
+
+    public AudioClip Bleep;
+    private AudioSource audioPlayer;
+
+    private VideoPlayer videoPlayer;
+
+    GameObject tv;
+
+    float Timer = 30f;
+
+    bool startTimer = true;
+
+    void Start()
+    {
+       
+        tv = transform.GetChild(0).gameObject;
+        audioPlayer = GetComponent<AudioSource>();
+        videoPlayer = tv.GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        if (startTimer == true)
+        {
+            Timer = Timer - Time.deltaTime;
+            Debug.Log(Timer);
+        }
+
+        if (Timer <= 0 && startTimer == true)
+        {
+            videoPlayer.Play();
+            startTimer = false;
+            if (!audioPlayer.isPlaying)
+            {
+                audioPlayer.clip = Bleep;
+                audioPlayer.Play();
+            }
+        }
+        
+    }
+}
+
