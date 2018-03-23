8d0eeaa31e7a2b7399630f7bde333840c318f378
FightAH <34405770+FightAH@users.noreply.github.com>
2018-03-23 13:08:49 +0100

    Tutorial scene with video
    Tutorial scene with video

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class NextScene : MonoBehaviour
+{
+
+    public AudioClip Bleep;
+    private AudioSource audioPlayer;
+
+    private VideoPlayer videoPlayer;
+
+    GameObject tv;
+
+    float Timer = 110f;
+
+    // Use this for initialization
+    void Start()
+    {
+        tv = transform.GetChild(0).gameObject;
+        audioPlayer = GetComponent<AudioSource>();
+        videoPlayer = tv.GetComponent<VideoPlayer>();
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer = Timer - Time.deltaTime;
+        Debug.Log(Timer);
+
+        if (Timer <= 0)
+        {
+            SceneManager.LoadScene("Appartment");
+            Debug.Log("Load");
+        }
+    }
+}
