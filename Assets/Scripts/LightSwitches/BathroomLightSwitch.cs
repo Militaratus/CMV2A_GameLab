676894ead39323225bef7b0e42c777006357c9c4
FightAH <34405770+FightAH@users.noreply.github.com>
2018-04-23 13:29:42 +0200

    Lights working

---
+++ Assets/Scripts/LightSwitches/BathroomLightSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.Video;
+
+public class BathroomLightSwitch : VRTK_InteractableObject
+{
+
+    public AudioClip Bleep;
+    private AudioSource audioPlayer;
+
+    GameObject Lights;
+
+    int lightOff = 1;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log("Using");
+        if (lightOff == 1)
+        {
+            Lights.SetActive(true);
+            if (!audioPlayer.isPlaying)
+            {
+                audioPlayer.clip = Bleep;
+                audioPlayer.Play();
+            }
+            lightOff = 0;
+        }
+        else if (lightOff == 0)
+        {
+            Lights.SetActive(false);
+            audioPlayer.Play();
+            lightOff = 1;
+        }
+    }
+
+    protected void Start()
+    {
+        Lights = transform.GetChild(0).gameObject;
+        audioPlayer = GetComponent<AudioSource>();
+    }
+}
---
+++ Assets/Scripts/LightSwitches/MainLightSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.Video;
+
+public class MainLightSwitch : VRTK_InteractableObject
+{
+
+    public AudioClip Bleep;
+    private AudioSource audioPlayer;
+
+    GameObject Lights;
+
+    int lightOff = 0;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        Debug.Log("Using");
+        if (lightOff == 0)
+        {
+            Lights.SetActive(false);
+            if (!audioPlayer.isPlaying)
+            {
+                audioPlayer.clip = Bleep;
+                audioPlayer.Play();
+            }
+            lightOff = 1;
+        }
+        else if (lightOff == 1)
+        {
+            Lights.SetActive(true);
+            audioPlayer.Play();
+            lightOff = 0;
+        }
+    }
+
+    protected void Start()
+    {
+        Lights = transform.GetChild(0).gameObject;
+        audioPlayer = GetComponent<AudioSource>();
+    }
+}
