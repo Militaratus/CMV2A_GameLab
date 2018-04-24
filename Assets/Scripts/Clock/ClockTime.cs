32ad40b3c83f20d91ed77507393979a668f8f6c0
FightAH <34405770+FightAH@users.noreply.github.com>
2018-04-24 09:46:25 +0200

    Added clock time
    Added clock time

---
+++ Assets/Scripts/Clock/ClockTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockTime : MonoBehaviour {
+
+    string time;
+
+    public Text clockText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        time = System.DateTime.Now.ToString("HH:mm");
+        Debug.Log(time);
+        clockText.text = time;
+    }
+}
