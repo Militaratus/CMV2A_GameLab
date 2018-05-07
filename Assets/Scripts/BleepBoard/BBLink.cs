3fde6a44470ea5acd4ca212c0d088b0a2a9658b1
Militaratus <jarednealon@outlook.com>
2018-05-07 11:11:58 +0200

    BleepBoard V0.01

---
+++ Assets/Scripts/BleepBoard/BBLink.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BBLink : MonoBehaviour
+{
+    public Transform lineTarget;
+    private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (lineRenderer == null)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+
+        if (lineTarget != null)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, lineTarget.position);
+
+            if (lineRenderer.material == null)
+            {
+                Shader shader = new Shader();
+                Material myMaterial = new Material(shader);
+                myMaterial.color = Color.green;
+                lineRenderer.material = myMaterial;
+            }
+
+            lineRenderer.material.color = Color.green;
+        }
+	}
+}
