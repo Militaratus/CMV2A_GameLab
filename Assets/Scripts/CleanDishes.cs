dc1d80600b5a8997c1364f617362a4b36844ab03
FightAH <34405770+FightAH@users.noreply.github.com>
2018-06-10 17:13:35 +0200

    Do the dishes

---
+++ Assets/Scripts/CleanDishes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanDishes : MonoBehaviour {
+
+    MeshRenderer renderer;
+
+    public Material materialClean;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.name == "Plate dirtyNEW")
+        {
+            renderer = col.transform.GetComponent<MeshRenderer>();
+            renderer.material = materialClean;
+            Debug.Log(col);
+        }
+        if (col.name == "Dirty Mug NEW")
+        {
+            renderer = col.transform.GetComponent<MeshRenderer>();
+            renderer.material = materialClean;
+            Debug.Log(col);
+        }
+    }
+}
